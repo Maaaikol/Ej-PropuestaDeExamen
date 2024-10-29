89cb2b5887232cff237bbbf69ee8e923a8aea06f
Maaaikol <mreachu@myuax.com>
2024-10-29 11:12:16 +0100

    TRIPLES PITAGORICOS

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/micha/Desktop/propuesta de examen(programación)/Ej-PropuestaDeExamen
+# Install script for directory: C:/Users/micha/Desktop/Nueva carpeta (3)/Ej-PropuestaDeExamen
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/micha/Desktop/propuesta de examen(programación)/Ej-PropuestaDeExamen/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/micha/Desktop/Nueva carpeta (3)/Ej-PropuestaDeExamen/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -1,27 +1,21 @@
 #include <iostream>
-#include <cmath>  // Para usar la función sqrt
+#include <cmath>
+using namespace std;
+
+int n;
 
 int main() {
     int a, b, c;
-
-    // Iteramos sobre los posibles valores de 'a' y 'b'
-    for (a = 1; a <= 20; a++) {   // 'a' va desde 1 hasta 20
-        for (b = a; b <= 20; b++) {   // 'b' va desde 'a' hasta 20
-            // Calculamos 'c' usando la fórmula pitagórica
-            double c_f = sqrt(a * a + b * b);  // c_f es el valor flotante de c
-
-            // Verificamos si 'c' es un número entero
+    for (a = 1; a <= n; a++) {
+        for (b = a; b <= n; b++) {
+            double c_f = sqrt(a*a + b*b);
             if (c_f == static_cast<int>(c_f)) {
-                c = static_cast<int>(c_f);  // Convertimos c_f a entero
-
-                // Verificamos la condición del triángulo: a + b > c, a + c > b, b + c > a
+                c = static_cast<int>(c_f);
                 if (a + b > c && a + c > b && b + c > a) {
-                    // Imprimimos el triple pitagórico
-                    std::cout << "Triple pitagórico: " << a << " - " << b << " - " << c << std::endl;
+                    cout << "Triple pitagórico: " << a << " - " << b << " - " << c << endl;
                 }
             }
         }
     }
-
     return 0;
 }
